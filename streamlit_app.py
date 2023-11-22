0629d1480a2df44decac5fbd0feaad70f7dca0fc
NikitaKorablev <kit06092003@gmail.com>
2023-11-22 21:52:15 +0300

    streamlit2

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ import numpy as np
 import pydeck as pdk
 
 # df = pd.DataFrame(np.random.randn(10, 20), columns=("col %d" % i for i in range(20)))
-df = pd.read_csv("WA_Fn-UseC_-Telco-Customer-Churn.csv")
+df = pd.read_csv("../WA_Fn-UseC_-Telco-Customer-Churn.csv")
 
 st.dataframe(df)
 # st.dataframe(df.style.highlight_max(axis=0))
